451727da9e0fc845a505325a2cb5aab81ffdfe65
siga-siga <si5415@outlook.jp>
2020-10-25 14:28:51 +0900

    ルームマイグレーションの修正

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2020_10_24_150217) do
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "room", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
